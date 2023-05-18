ecfd21c3cb94a561fe52204f9859f58d80e6d9b1
caducasara <carloscasara98@gmail.com>
2023-05-18 01:07:25 -0300

    feat: init config server CRUD

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,16 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
 
 const PORT = 3333
 const URL = 'http://localhost:'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true, // todas URL's de Front-End poderam acessar as rotas
 })
+app.register(memoriesRoutes)
 
 app
   .listen({
